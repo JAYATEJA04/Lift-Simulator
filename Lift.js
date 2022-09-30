79ba4a0ab5ddfd039970ea34394f5985879e828c
Jaya <akulajayateja4444@gmail.com>
2022-09-30 16:09:10 +0530

    added the functionaliy of floors button

--- Lift.js
+++ Lift.js
@@ -1,4 +1,23 @@
-const floor = document.querySelector('add-floor-button');
-floor.addEventListener('click', function(){
-    console.log("Floor");
-})
+const floor = document.getElementById('floor');
+const lift = document.getElementById('lift');
+let count = 0;
+
+function Floorbtn(){
+    if(count > 10){
+        alert('the maximum no.of floors should be <= 10');
+    } else {
+        let floorsBTN = document.createElement("div");
+        floorsBTN.setAttribute("class", "floors");
+
+        floorsBTN.innerHTML = `
+            <div class="floor" >
+                <div id="lift-buttons">
+                    <button id="call-up">UP</button>
+                    <button id="call-down">DOWN</button>
+                </div>
+            </div>
+        `
+        document.body.appendChild(floorsBTN)
+        count++;
+    }
+}
