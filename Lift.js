b3501414bcf136fc77d67a74c3d85164eeded1ef
Jaya <akulajayateja4444@gmail.com>
2022-10-06 01:11:42 +0530

    Added preventDefault stage

--- Lift.js
+++ Lift.js
@@ -1,23 +1,33 @@
-const floor = document.getElementById('floor');
-const lift = document.getElementById('lift');
-let count = 0;
+let floorInput = document.querySelector('.floor-input');
+let liftInput = document.querySelector('.lift-input');
+let confirmButton = document.querySelector('.confirm-btn');
+let countFloors = 0;
 
-function Floorbtn(){
-    if(count > 10){
-        alert('the maximum no.of floors should be <= 10');
-    } else {
-        let floorsBTN = document.createElement("div");
-        floorsBTN.setAttribute("class", "floors");
+// Adding floors and preventDefault.
+confirmButton.addEventListener('click', function(e){
+        e.preventDefault();
+    }
+)
+function confirmClick(e){
+    for(let i = 0 ; i < floorInput.value ; i++){
+        if(floorInput.value > 10){
+            alert(`Enter the no.of floors in the range of 1 to 10.`)
+            break;
+        } else {
+            
+            let floorsBTN = document.createElement("div");
+            floorsBTN.setAttribute("class", "floors");
 
-        floorsBTN.innerHTML = `
-            <div class="floor" >
-                <div id="lift-buttons">
-                    <button id="call-up">UP</button>
-                    <button id="call-down">DOWN</button>
+            floorsBTN.innerHTML = `
+                <div class="floor" >
+                    <div id="lift-buttons">
+                        <button id="call-up">UP</button>
+                        <button id="call-down">DOWN</button>
+                    </div>
                 </div>
-            </div>
-        `
-        document.body.appendChild(floorsBTN)
-        count++;
+            `
+            document.body.appendChild(floorsBTN);
+            countFloors++;
+        }
     }
 }
