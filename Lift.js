f41845841156151dbd0277d8502545e8f99ce57b
Jaya <akulajayateja4444@gmail.com>
2022-10-06 21:00:37 +0530

    made changes in JS file, now we are able to add floors and lifts dynamically. This when I figured out how to add lifts in 0th floor. Eureka moment.

--- Lift.js
+++ Lift.js
@@ -6,23 +6,42 @@ let countFloors = 0;
 // Adding floors and preventDefault.
 confirmButton.addEventListener('click', function(e){
         e.preventDefault();
+        generateFloors(floorInput.value);
+        generateLifts(liftInput.value);
     }
 )
-function confirmClick(){
-    for(let i = 0 ; i < floorInput.value ; i++){
-            let floorsBTN = document.createElement("div");
-            floorsBTN.setAttribute("class", "floors");
 
-            floorsBTN.innerHTML = `
-                <div class="floor" >
-                    <div id="lift-buttons">
-                        <button id="call-up">UP</button>
-                        <button id="call-down">DOWN</button>
-                        <p>${countFloors} </p>
-                    </div>
-                </div>
-            `
-            document.body.appendChild(floorsBTN);
-            countFloors++;
+function generateFloors(numberOfFloors){
+    document.querySelector('.lift-simulation').innerHTML = ``;
+    for(let i = 0 ; i < numberOfFloors ; i++){
+        let floorNo = `Level-${numberOfFloors - i - 1}`
+        let currentFloor = document.createElement("div");
+        currentFloor.setAttribute('id',floorNo)
+
+        currentFloor.setAttribute("class", "floor");
+        currentFloor.innerHTML = `
+        <div class="upward-downward">
+            <button class="UP-btn">UP</button>
+            <button class="Down-btn">DOWN</button>
+        </div>
+        `
+        document.querySelector('.lift-simulation').appendChild(currentFloor);
+    }
+}
+
+function generateLifts(numberOfLifts){
+    for(let i = 0 ; i < numberOfLifts ; i++){
+        let liftCount = `Lift-${i}`
+        const currentLift = document.createElement('div');
+        currentLift.setAttribute('id',liftCount);
+
+        // currentLift.setAttribute("class", "lifts");
+        currentLift.innerHTML = `
+            <div class="lifts">
+                <div class="lift-left-door FLEX"></div>
+                <div class="lift-right-door FLEX"></div>
+            </div>
+        `
+        document.getElementById('Level-0').appendChild(currentLift);
     }
 }
