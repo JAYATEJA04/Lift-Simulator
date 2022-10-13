f545878e0fd73dc720abb69f51995931321c4515
Jaya <akulajayateja4444@gmail.com>
2022-10-13 19:11:37 +0530

    deleted most of the code as it was not making any sense. Starting it again

--- Lift.js
+++ Lift.js
@@ -1,18 +1,6 @@
 let floorInput = document.querySelector('.floor-input');
 let liftInput = document.querySelector('.lift-input');
 let confirmButton = document.querySelector('.confirm-btn');
-let destinationArr = [];
-let liftQueue = [];
-let liftPosition = [];
-
-// Adding floors and preventDefault.
-confirmButton.addEventListener('click', function(e){
-        e.preventDefault();
-        generateFloors(floorInput.value);
-        generateLifts(liftInput.value);
-        floorButtons();
-    }
-)
 
 function generateFloors(numberOfFloors){
     if(numberOfFloors <= 0 || numberOfFloors > 10){
@@ -20,15 +8,15 @@ function generateFloors(numberOfFloors){
     } else {
         document.querySelector('.lift-simulation').innerHTML = ``;
         for(let i = 0 ; i < numberOfFloors ; i++){
-        let floorNumber = `Floor-${numberOfFloors - i - 1}`
+        let floorNumber = `Floor-${numberOfFloors - i - 1}`;
         let currentFloor = document.createElement("div");
         currentFloor.setAttribute('id',floorNumber)
 
         currentFloor.setAttribute("class", "floor");
         currentFloor.innerHTML = `
         <div class="upward-downward">
-            <button id=${currentFloor} class="floor-btn UP-btn">UP</button>
-            <button id=${currentFloor} class="floor-btn Down-btn">DOWN</button>
+            <button class="floor-btn UP-btn">UP</button>
+            <button class="floor-btn Down-btn">DOWN</button>
             <p class="Floor-number"> ${floorNumber} </p>
         </div>
         `
@@ -46,6 +34,8 @@ function generateLifts(numberOfLifts){
             const currentLift = document.createElement('div');
             currentLift.setAttribute('id',liftCount);
     
+            liftMotionDetail = false;
+            liftNumberDetail = liftCount;
             // currentLift.setAttribute("class", "lifts");
             currentLift.innerHTML = `
                 <div class="lifts">
@@ -58,16 +48,9 @@ function generateLifts(numberOfLifts){
         }
     }
 }
-
-function floorButtons(){
-    let floorsButton = document.querySelector('.floor-btn');
-    floorsButton.forEach(f => {
-        f.addEventListener('click', () => {
-            let targetFloor = parseInt(f.id.slice(-1));
-            if(!destinationArr.includes(targetFloor)){
-                destinationArr.push(targetFloor);
-                liftQueue.push(targetFloor);
-            }
-        })
-    })
-}
+// Adding floors and preventDefault.
+confirmButton.addEventListener('click', function(e){
+    e.preventDefault();
+    generateFloors(floorInput.value);
+    generateLifts(liftInput.value);
+})
